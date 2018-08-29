67ea583deba5bd649b58b9ae5e5109c55ec2ebc4
Burkey Koontz <bk.koontz@gmail.com>
2018-08-29 08:40:10 -0700

    added timer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Input from './Input';
+import Timer from './Timer';
 
 export default class App extends React.Component {
   constructor(props){
@@ -11,7 +12,10 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Input />
+      <div>
+        <Timer />
+        <Input />
+      </div>
     );
 
   }
---
+++ src/Timer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+export default function Timer(){
+
+  return (
+    <h1>10:00</h1>
+  )
+
+}
