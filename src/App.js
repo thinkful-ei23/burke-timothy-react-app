551dac97c0dc5537956a0a1016f4f0f3c976969c
Burkey Koontz <bk.koontz@gmail.com>
2018-08-29 08:29:35 -0700

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+'use strict';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
   }
-}
 
-export default App;
+  render() {
+    return <p>It's working</p>;
+  }
+
+
+}
