8d8764bde34acf99d60669caa5c6c3fad7692487
Burkey Koontz <bk.koontz@gmail.com>
2018-08-29 09:17:01 -0700

    state is updated on submit

--- src/Input.js
+++ src/Input.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-export default function Input() {
+export default class Input extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    }
+  }
+
+  handleSubmit(e, time) {
+    e.preventDefault();
+    this.props.setTime(time);
+  }
+
+  handleInput(input) {
+    this.setState({input});
+  }
+
+  render () {
   return (
-    <form className="input" onSubmit>
+    <form className="input" onSubmit={(e) => this.handleSubmit(e, this.state.input)}>
       <label htmlFor="input">Set timer</label>
-      <input id="input" type="number" />
+      <input onChange={(e) => this.handleInput(e.target.value)} id="input" type="number" />
       <input type="submit" value="Start timer" />
     </form>
   );
+}
 }
